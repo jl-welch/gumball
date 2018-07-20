36a6a4e88db7ad4f838f6386c6127349c1dd1b4d
jl-welch <jlw.dev@outlook.com>
2018-07-20 23:04:59 +0100

    add closest polyfill

--- dist/javascripts/gumball.js
+++ dist/javascripts/gumball.js
@@ -10,7 +10,19 @@ if (window.NodeList && !NodeList.prototype.forEach) {
 }
 
 if (!Element.prototype.matches) {
-  Element.prototype.matches = Element.prototype.msMatchesSelector;
+  Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function (s) {
+    var el = this;
+    if (!document.documentElement.contains(el)) return null;
+    do {
+      if (el.matches(s)) return el;
+      el = el.parentElement || el.parentNode;
+    } while (el !== null && el.nodeType === 1);
+    return null;
+  };
 }
 // from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
 (function (arr) {
